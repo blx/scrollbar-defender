ad1e527f503048bc5f08fc01c445b26e7cb40ab4
Ben Cook <b@bencook.ca>
2015-07-17 05:38:30 -0700

    go bookmarklet route for now

--- chrome-fucking-scrollbar-defender.js
+++
@@ -1,7 +0,0 @@
-
-chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.executeScript({
-        code:"document.onmousewheel=null;window.onmousewheel=null;window.onwheel=null;"
-    });
-});
-
